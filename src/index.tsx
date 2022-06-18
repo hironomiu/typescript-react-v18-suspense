25fda2bc22773f1d30cbbb7f95ca54192a118d73
hironomiu <hironomiu@gmail.com>
2022-06-19 07:32:11 +0900

    commited

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
-import * as ReactDOMClient from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-const rootElement = document.getElementById('root')
-if (rootElement) ReactDOMClient.createRoot(rootElement).render(<App />)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
