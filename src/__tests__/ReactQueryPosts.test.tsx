67179d3c7c6a1d2f23b68cf0b8d58a43b6703e0e
hironomiu <hironomiu@gmail.com>
2022-06-19 15:45:47 +0900

    commited

--- src/__tests__/ReactQueryPosts.test.tsx
+++ src/__tests__/ReactQueryPosts.test.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react'
 import { screen, render } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
@@ -35,10 +36,13 @@ describe('ReactQueryPost', () => {
     render(
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
-          <ReactQueryPosts />
+          <Suspense fallback={<div>loading ...</div>}>
+            <ReactQueryPosts />
+          </Suspense>
         </BrowserRouter>
       </QueryClientProvider>
     )
+    expect(screen.getByText('loading ...')).toBeInTheDocument()
     expect(
       await screen.findByText('Fetched ReactQueryPost')
     ).toBeInTheDocument()
---
+++ src/__tests__/ReactQueryUsers.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import ReactQueryUsers from '../components/ReactQueryUsers'
+import { setupServer } from 'msw/node'
+import { handlers } from '../mock/handlers'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+      suspense: true,
+    },
+  },
+})
+
+const server = setupServer(...handlers)
+
+beforeAll(() => {
+  server.listen()
+})
+
+describe('ReactQueryUsers', () => {
+  it('test', async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryUsers />
+      </QueryClientProvider>
+    )
+    expect(
+      await screen.findByText('Fetched ReactQueryUsers')
+    ).toBeInTheDocument()
+  })
+})
